8dc0adaf180ff5fef49f966a4838775687f309e9
SzymonSzmydt <szymonszmydt@gmail.com>
2022-12-01 19:32:16 +0100

    fix form validation

--- src/pages/contact/Form.tsx
+++ src/pages/contact/Form.tsx
@@ -23,7 +23,7 @@ export function Form() {
                         fullName: "",
                         email: "",
                         message: "",
-                        error: "",
+                        math: "",
                         success: "Success!",
                     });
                 },
