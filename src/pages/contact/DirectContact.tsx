7144861f70cf75be0dcdef6c0bf6b1bc0ba0b4c3
SzymonSzmydt <szymonszmydt@gmail.com>
2022-12-08 12:44:55 +0100

    DirectContact cut content

--- src/pages/contact/DirectContact.tsx
+++ src/pages/contact/DirectContact.tsx
@@ -1,34 +1,16 @@
 import './css/contact.css'
+import { NavLink } from 'react-router-dom';
 export function DirectContact() {
     return (
         <section className="flex direct">
-            <div className="direct__wrapper">
-                <h3> Zapraszam do kontaktu</h3>
-                <div className="flex direct__wrapper-box">
-                    <div className="symbol phone phone-filter" />
-                    <a href="tel:663 617 707">Zadzwoń</a>
-                </div>
-                <div className="flex direct__wrapper-box">
-                    <div className="symbol mail phone-filter" />
-                    <a href="mailto:szymonszmydt@gmail.com">Napisz e-mail</a>
-                </div>
-                <div className="flex direct__wrapper-box">
-                    <div className="symbol linkedin phone-filter" />
-                    <a
-                        href="https://www.linkedin.com/in/szymon-szmydt-5b0992230/"
-                        target="_blank"
-                        rel="noreferrer">
-                        Znajdź mnie na LinkedIn
-                    </a>
-                </div>
-                <div className="flex direct__wrapper-box">
-                    <p>
-                        <strong> 
-                            Zachęcam do skorzystania z formularza
-                        </strong>
-                    </p>
-                </div>
-            </div>
+            <h3> Zapraszam do kontaktu</h3>
+            <p>
+                Proszę czuć się swobodnie korzystając z formularza kontaktowego. 
+                Na każdą wiadomość odpowiem.
+            </p><br/>
+            <p>
+                Z wyrazami szacunku <NavLink to="/">nearState</NavLink>
+            </p>
         </section>
     );
 }
