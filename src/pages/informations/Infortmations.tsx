f008a0240ab96350702fbca119b7793bf1ef5c14
SzymonSzmydt <szymonszmydt@gmail.com>
2022-12-12 20:12:28 +0100

    fix RWD display

--- src/pages/informations/Infortmations.tsx
+++ src/pages/informations/Infortmations.tsx
@@ -1,25 +1,27 @@
-import './css/information.css';
-import { WindowModule } from '../../components/window/WindowModule';
-import { Terms } from './Terms';
+import "./css/information.css";
+import { WindowModule } from "../../components/window/WindowModule";
+import { Terms } from "./Terms";
+import { Window } from "../../components/window/Window";
 export function Informations() {
-    return (
-        <div className="container-lg informations">
-            <WindowModule>
-                <h2>Informacje dotyczące danych na stronie</h2>
-                <p>
-                    Dane dotyczące jakości powietrza pobierane są ze
-                    Światowego Indeksu Jakości Powietrza. Dane te
-                    publikowane są w czasie rzeczywistym, dlatego nie mogą być
-                    potwierdzone w momencie publikacji. Jednak w celu
-                    zapewnienia wysokiego poziomu dokładności dla każdej
-                    wartości AQI, stosuje się kilka rozwiązań do przetwarzania
-                    uczenia maszynowego. Na przykład weryfikowana jest spójność
-                    danych w czasie rzeczywistym z&nbsp;sąsiednimi stacjami, co
-                    pozwala automatycznie wykrywać wadliwe stacje monitorujące
-                    i&nbsp;w&nbsp;razie potrzeby usuwać je z&nbsp;mapy.
-                </p>
-            </WindowModule>
-            <Terms />
-        </div>
-    );
+  return (
+    <Window>
+      <div className="informations container-lg">
+        <WindowModule>
+          <h2>Informacje dotyczące danych na stronie</h2>
+          <p>
+            Dane dotyczące jakości powietrza pobierane są ze&nbsp;Światowego Indeksu
+            Jakości Powietrza. Dane te publikowane są w&nbsp;czasie rzeczywistym,
+            dlatego nie mogą być potwierdzone w&nbsp;momencie publikacji. Jednak 
+            w&nbsp;celu zapewnienia wysokiego poziomu dokładności dla każdej wartości
+            AQI, stosuje się&nbsp;kilka rozwiązań do&nbsp;przetwarzania uczenia
+            maszynowego. Na&nbsp;przykład weryfikowana jest spójność danych w&nbsp;czasie
+            rzeczywistym z&nbsp;sąsiednimi stacjami, co&nbsp;pozwala automatycznie
+            wykrywać wadliwe stacje monitorujące i&nbsp;w&nbsp;razie potrzeby
+            usuwać je&nbsp;z&nbsp;mapy.
+          </p>
+        </WindowModule>
+      </div>
+      <Terms />
+    </Window>
+  );
 }
