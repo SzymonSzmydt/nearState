d7c756b6e4f2a782996d1a661881d2dac57e02c3
SzymonSzmydt <szymonszmydt@gmail.com>
2022-12-15 14:07:10 +0100

    fix article tag in AirQuality

--- src/components/ui/airQuality/AirQuality.tsx
+++ src/components/ui/airQuality/AirQuality.tsx
@@ -1,65 +1,73 @@
-import './css/ariQuality.css';
-import { WindowModule } from '../../../components/window/WindowModule';
-import { Chemical } from '../../../components/ui/popup/Chemical';
-import { chemicals } from '../../../contex/ChemicalData';
-import { legend } from '../../../contex/types/AirLegendDescription';
-import { PopUpLegend } from '../../../components/ui/popup/PopUpLegend';
-import { useDispatch, useSelector } from 'react-redux';
-import { popUpOff, legendPopUp, chemicalPopUp, indexPopUp } from '../../../contex/redux/PopUpLogic';
-import { RootState } from '../../../contex/redux/store';
+import "./css/ariQuality.css";
+import { WindowModule } from "../../../components/window/WindowModule";
+import { Chemical } from "../../../components/ui/popup/Chemical";
+import { chemicals } from "../../../contex/ChemicalData";
+import { legend } from "../../../contex/types/AirLegendDescription";
+import { PopUpLegend } from "../../../components/ui/popup/PopUpLegend";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  popUpOff,
+  legendPopUp,
+  chemicalPopUp,
+  indexPopUp,
+} from "../../../contex/redux/PopUpLogic";
+import { RootState } from "../../../contex/redux/store";
 type AirQualityProps = {
-    title: string;
-}
+  title: string;
+};
 export function AirQuality({ title }: AirQualityProps) {
-    const dispatch = useDispatch();
-    const chemicalPop = useSelector((state: RootState) => state.popUp.chemical);
-    const legendPop = useSelector((state: RootState) => state.popUp.legend);
-    const indexPop = useSelector((state: RootState) => state.popUp.index);
-    const handleClick = (index: number, fn: 'chemical' | 'legend') => {
-        dispatch(popUpOff());
-        if (fn && fn === 'chemical') dispatch(chemicalPopUp(true));
-        else if (fn && fn === 'legend') dispatch(legendPopUp(true))
-        dispatch(indexPopUp(index));
-    }
-    return (
-        <WindowModule>
-                <h1 className="h1"> { title }
-                    <span className="small-font"> - US AQI </span>
-                </h1>
-                <section className="flex wrap legend__section">
-                    { legend.map( (element, index) => 
-                    <div 
-                        key={ element.rank } 
-                        className="flex legend" 
-                        style={{ backgroundColor: element.bgcolor }}
-                        onClick={ ()=> handleClick(index, 'legend') }>
-                        <span> { element.rank } </span>
-                        <span> { element.range } </span>
-                    </div> )}
-                    { legendPop ? 
-                    <PopUpLegend {...legend[indexPop]} /> 
-                    : null }
-                </section>
-                <section>
-                    <h2>AQI (ang. Air Quality Index)</h2>
-                    <article>
-                        Jest to ocena jakości powietrza, która obejmuje poziom
-                        pyłów zawieszonych PM2.5 i PM10 oraz niektórych
-                        zanieczyszczeń gazowych, takich jak:
-                        <div className="weather_quality-list">
-                            <ul className="weather_quality-list-ul">
-                              { chemicals.map((item, index) => (
-                                <li key={item.name} onClick={()=> handleClick(index, 'chemical')}>
-                                    { item.name}
-                                </li>
-                                ))}
-                            </ul>
-                            { chemicalPop ? 
-                                <Chemical compound={chemicals[indexPop]}/> : null }
-                        </div>
-                        które często są wynikiem spalania paliw.
-                    </article>
-                </section>
-        </WindowModule>
-    );
+  const dispatch = useDispatch();
+  const chemicalPop = useSelector((state: RootState) => state.popUp.chemical);
+  const legendPop = useSelector((state: RootState) => state.popUp.legend);
+  const indexPop = useSelector((state: RootState) => state.popUp.index);
+  const handleClick = (index: number, fn: "chemical" | "legend") => {
+    dispatch(popUpOff());
+    if (fn && fn === "chemical") dispatch(chemicalPopUp(true));
+    else if (fn && fn === "legend") dispatch(legendPopUp(true));
+    dispatch(indexPopUp(index));
+  };
+  return (
+    <WindowModule>
+      <article>
+        <h1 className="h1">
+          {title}
+          <span className="small-font"> - US AQI </span>
+        </h1>
+        <section className="flex wrap legend__section">
+          {legend.map((element, index) => (
+            <div
+              key={element.rank}
+              className="flex legend"
+              style={{ backgroundColor: element.bgcolor }}
+              onClick={() => handleClick(index, "legend")}
+            >
+              <span> {element.rank} </span>
+              <span> {element.range} </span>
+            </div>
+          ))}
+          {legendPop ? <PopUpLegend {...legend[indexPop]} /> : null}
+        </section>
+        <>
+          <h2>AQI (ang. Air Quality Index)</h2>
+          Jest to ocena jakości powietrza, która obejmuje poziom pyłów
+          zawieszonych PM2.5 i PM10 oraz niektórych zanieczyszczeń gazowych,
+          takich jak:
+          <div className="weather_quality-list">
+            <ul className="weather_quality-list-ul">
+              {chemicals.map((item, index) => (
+                <li
+                  key={item.name}
+                  onClick={() => handleClick(index, "chemical")}
+                >
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+            {chemicalPop ? <Chemical compound={chemicals[indexPop]} /> : null}
+          </div>
+          które często są wynikiem spalania paliw.
+        </>
+      </article>
+    </WindowModule>
+  );
 }
