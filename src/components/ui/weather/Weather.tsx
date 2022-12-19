b73be433d38c4406ab3b9bd5b6460a1fd1b381f0
SzymonSzmydt <szymonszmydt@gmail.com>
2022-12-19 10:39:28 +0100

    deploy production

--- src/components/ui/weather/Weather.tsx
+++ src/components/ui/weather/Weather.tsx
@@ -7,7 +7,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAirCityData } from '../../../contex/redux/AirCitySlice';
 import { useEffect, useCallback } from 'react';
 import { RootState } from '../../../contex/redux/store';
-const data = JSON.parse(localStorage.getItem('wroclaw') || "");
+// const data = JSON.parse(localStorage.getItem('wroclaw') || "");
 export function Weather() {
     const airData = useSelector((state: RootState) => state.airCity.data);
     const dispatch = useDispatch();
@@ -17,16 +17,16 @@ export function Weather() {
             const response = await fetch(process.env.REACT_APP_urlNearest as string);
             const data = await response.json();
             dispatch(getAirCityData(data.data));
-            localStorage.setItem('wroclaw', JSON.stringify(data));
+            // localStorage.setItem('wroclaw', JSON.stringify(data));
         } catch (err) {
             console.error(err);
         }
     }, [process.env.REACT_APP_urlNearest]);
     useEffect(()=> {
-    //     if (city !== 'Wroclaw') {
-    //         fetchData();
-    //     }
-    dispatch(getAirCityData(data));
+        if (city !== 'Wroclaw') {
+            fetchData();
+        }
+    // dispatch(getAirCityData(data));
     }, []);
     return (
         <>
