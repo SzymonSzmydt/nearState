7166e048e18239f2e1b9d21de03ffbf78c228582
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-09 17:04:26 +0100

    removing React.StricktMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './contex/redux/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./contex/redux/store";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-  Route
+  Route,
 } from "react-router-dom";
-import 'normalize.css';
-import './index.css';
-import App from './App';
-import { Home } from './pages/home/Home';
-import { Contact } from './pages/contact/Contact';
-import { Informations } from './pages/informations/Infortmations';
-import { Europe } from './pages/europe/Europe';
+import "normalize.css";
+import "./index.css";
+import App from "./App";
+import { Home } from "./pages/home/Home";
+import { Contact } from "./pages/contact/Contact";
+import { Informations } from "./pages/informations/Infortmations";
+import { Europe } from "./pages/europe/Europe";
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path="/" element={<App />} errorElement={<App />}>
-                <Route index={true} element={<Home />} />
-                <Route path="europe" element={ <Europe /> } />
-                <Route path="contact" element={<Contact />} />
-                <Route path="informations" element={<Informations />} />
-            </Route>
-            <Route path="*" element={<App />} />
-        </>
-    )
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} errorElement={<App />}>
+        <Route index={true} element={<Home />} />
+        <Route path="europe" element={<Europe />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="informations" element={<Informations />} />
+      </Route>
+      <Route path="*" element={<App />} />
+    </>
+  )
 );
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
