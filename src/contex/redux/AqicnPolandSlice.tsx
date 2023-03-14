8344e57e8290f17ac05a924360e9d3866b7f5733
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-14 17:01:14 +0100

    change city from Poznan to Torun

--- src/contex/redux/AqicnPolandSlice.tsx
+++ src/contex/redux/AqicnPolandSlice.tsx
@@ -1,22 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import { Aqicn, AqicnType } from '../types/Aqicn';
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { Aqicn, AqicnType } from "../types/Aqicn";
 const initialState: AqicnType = {
   value: [],
   isLoaded: false,
-  citys: ["Katowice", "Poznań", "Warszawa", "Gdansk", "Kraków", "Łódź", "Szczecin", "Wrocław"]
-}
+  citys: [
+    "Katowice",
+    "Toruń",
+    "Warszawa",
+    "Gdansk",
+    "Kraków",
+    "Łódź",
+    "Szczecin",
+    "Wrocław",
+  ],
+};
 export const aqicnPoland = createSlice({
-  name: 'poland',
+  name: "poland",
   initialState,
   reducers: {
-    getPolandAqi: (state, action:PayloadAction<Array<Aqicn>>) => {
-      state.value = action.payload
+    getPolandAqi: (state, action: PayloadAction<Array<Aqicn>>) => {
+      state.value = action.payload;
     },
-    getPlLoaded: (state, action:PayloadAction<boolean>) => {
-        state.isLoaded = action.payload
+    getPlLoaded: (state, action: PayloadAction<boolean>) => {
+      state.isLoaded = action.payload;
     },
   },
-})
-export const { getPolandAqi, getPlLoaded } = aqicnPoland.actions
-export default aqicnPoland.reducer
+});
+export const { getPolandAqi, getPlLoaded } = aqicnPoland.actions;
+export default aqicnPoland.reducer;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,57 +1,61 @@
-import { Window } from './../../components/window/Window';
-import { Map } from './Map';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPolandAqi } from '../../contex/redux/AqicnPolandSlice';
-import { region } from '../../contex/redux/PopupLogic';
-import { useEffect } from 'react';
-import { RootState } from '../../contex/redux/store';
-// const data = JSON.parse(localStorage.getItem("allPromise") || "");
+import { Window } from "./../../components/window/Window";
+import { Map } from "./Map";
+import { useDispatch, useSelector } from "react-redux";
+import { getPolandAqi } from "../../contex/redux/AqicnPolandSlice";
+import { region } from "../../contex/redux/PopupLogic";
+import { useEffect } from "react";
+import { RootState } from "../../contex/redux/store";
 export function Home() {
-    const isLoaded = useSelector((state: RootState)=> state.poland.isLoaded);
-    const dispatch = useDispatch();
-    const dataFetch = async () => {
-        try {
-            const result = (
-                await Promise.all([
-                  fetch(process.env.REACT_APP_katowice as string),
-                  fetch(process.env.REACT_APP_poznan as string),
-                  fetch(process.env.REACT_APP_warszawa as string),
-                  fetch(process.env.REACT_APP_gdansk as string),
-                  fetch(process.env.REACT_APP_krakow as string),
-                  fetch(process.env.REACT_APP_lodz as string),
-                  fetch(process.env.REACT_APP_szczecin as string),
-                  fetch(process.env.REACT_APP_wroclaw as string)
-                ])
-              ).map((r) => r.json());
-              const [katowice, poznan, warszawa, gdansk, krakow, lodz, szczecin, wroclaw] = await Promise.all(
-                result
-              );
-              const data = [
-                katowice.data,
-                poznan.data, 
-                warszawa.data, 
-                gdansk.data, 
-                krakow.data, 
-                lodz.data, 
-                szczecin.data, 
-                wroclaw.data
-                ].sort((a, b) => b.aqi - a.aqi);
-              dispatch(getPolandAqi(data));
-            //   localStorage.setItem('allPromise', JSON.stringify(data));
-            } catch(err) {
-                console.log(err);  
-            }
-        }
-    useEffect(() => {
-        if (!isLoaded) {
-            dataFetch();
-        }
-      dispatch(region('poland'));
-    //   dispatch(getPolandAqi(data));
-    }, []);
-    return (
-        <Window>
-            <Map/>
-        </Window>
-    )
+  const isLoaded = useSelector((state: RootState) => state.poland.isLoaded);
+  const dispatch = useDispatch();
+  const dataFetch = async () => {
+    try {
+      const result = (
+        await Promise.all([
+          fetch(process.env.REACT_APP_katowice as string),
+          fetch(process.env.REACT_APP_torun as string),
+          fetch(process.env.REACT_APP_warszawa as string),
+          fetch(process.env.REACT_APP_gdansk as string),
+          fetch(process.env.REACT_APP_krakow as string),
+          fetch(process.env.REACT_APP_lodz as string),
+          fetch(process.env.REACT_APP_szczecin as string),
+          fetch(process.env.REACT_APP_wroclaw as string),
+        ])
+      ).map((r) => r.json());
+      const [
+        katowice,
+        torun,
+        warszawa,
+        gdansk,
+        krakow,
+        lodz,
+        szczecin,
+        wroclaw,
+      ] = await Promise.all(result);
+      const data = [
+        katowice.data,
+        torun.data,
+        warszawa.data,
+        gdansk.data,
+        krakow.data,
+        lodz.data,
+        szczecin.data,
+        wroclaw.data,
+      ].sort((a, b) => b.aqi - a.aqi);
+      dispatch(getPolandAqi(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    if (!isLoaded) {
+      dataFetch();
+    }
+    dispatch(region("poland"));
+  }, []);
+  return (
+    <Window>
+      <Map />
+    </Window>
+  );
 }
